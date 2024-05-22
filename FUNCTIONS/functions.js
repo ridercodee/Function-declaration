e0db1c279e9325d458c381adfa3b6b762a51db1c
Mehboob Khan <mehboobrider57@gmail.com>
2024-05-22 14:38:04 +0530

    Add files via upload

---
+++ FUNCTIONS/functions.js
@@ -0,0 +1,26 @@
+// Defining functions 
+
+function convertToLiters(gallons) { 
+  return gallons * 3.785;
+}
+
+console.log(convertToLiters(10));
+
+// function expression
+const convertToLiters = function(gallons){
+  return gallons * 3.785;
+}
+console.log(convertToLiters(10));
+
+
+
+// arrow function
+const convertToLiters = gallons => gallons * 3.785;
+console.log(convertToLiters(10));
+
+
+
+
+// function constructor
+const convertToLiters = new Function("gallons", 'return gallons * 3.785;');
+console.log(convertToLiters(10));
